ca2023e75b45a7ab456d920ff3fbf9412cc6b7a1
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-01-16 14:52:00 -0800

    Removed Comments

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -1,77 +1,57 @@
 ﻿using System;
 using System.IO;
-// This all exists in a PBT Namespace
 namespace PrincessBrideTrivia
 {
-    // We have a program object
     public class Program
     {
-        // The main class 
         public static void Main(string[] args)
         {
-            // First we obtain the file path
             string filePath = GetFilePath();
 
-            // Second we load the questions in an array from the file 
             Question[] questions = LoadQuestions(filePath);
 
-            // We init # of correct to 0 
             int numberCorrect = 0;
 
-            // We loop through each question 
             for (int i = 0; i < questions.Length; i++)
             {
-                // Asks the question and obtains whether the result is correct
                 bool result = AskQuestion(questions[i]);
-                // If it's correct, it increments the # of correct answers 
                 if (result)
                 {
                     numberCorrect++;
                 }
             }
-            // Outputs the percentage of answers that are correct
             Console.WriteLine($"You got {GetPercentCorrect(numberCorrect, questions.Length)} correct");
         }
-        // Uses an equation to get the percentage of the correct # of answers
         public static string GetPercentCorrect(double numberCorrectAnswers, double numberOfQuestions)
         {
             return (int)((numberCorrectAnswers / numberOfQuestions) * 100) + "%";
         }
 
-        // Asks the question to a user
         public static bool AskQuestion(Question question)
         {
-            // First displays a question
             DisplayQuestion(question);
 
-            // Receives a Guess from a user
             string userGuess = GetGuessFromUser();
 
-            // Displays the result of the user guess
             return DisplayResult(userGuess, question);
         }
 
-        // Reads the console to obtain the guess
         public static string GetGuessFromUser()
         {
             return Console.ReadLine();
         }
 
-        // Displays the result  of the guess
         public static bool DisplayResult(string userGuess, Question question)
         {
-            // If the user's guess is equal to the correct answer's index it tells them they're correct
             if (userGuess == question.CorrectAnswerIndex)
             {
                 Console.WriteLine("Correct");
                 return true;
             }
-            // Otherwise tells the user that it is false
             Console.WriteLine("Incorrect");
             return false;
         }
 
-        // Display Question is where it breaks and it is because the question object is null 
         public static void DisplayQuestion(Question question)
         {
             Console.WriteLine("Question: " + question.Text);
@@ -86,21 +66,16 @@ namespace PrincessBrideTrivia
             return "Trivia.txt";
         }
         
-        // Load questions must not be working correctly 
         public static Question[] LoadQuestions(string filePath)
         {
-            // Reads all of the lines from the filepath 
             string[] lines = File.ReadAllLines(filePath);
 
            
-            // The Array of Questions  
             Question[] questions = new Question[lines.Length / 5];
             for (int i = 0; i < questions.Length; i++)
             {
-                // Sets the line index to the next question each time, each question is 5 lines 
                 int lineIndex = i * 5;
                 string questionText = lines[lineIndex];
-                // First answer is line 1, and thus forth 
                 string answer1 = lines[lineIndex + 1];
                 string answer2 = lines[lineIndex + 2];
                 string answer3 = lines[lineIndex + 3];
