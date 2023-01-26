f1bdfb6cc676f1f6b69235b6cdc2075a69e531a5
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-01-26 10:44:43 -0800

    Removed Comment

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            //Feel free to use your own setup here - this is just provided as an example
             new Jester(new JokeOutput(), new JokeService()).TellJoke();
         }
     }
