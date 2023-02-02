979c4e9f95ba4eeab068b1a8c75b49d803126a49
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-02 11:15:25 -0800

    Removed Public Identifiers from Interfaces

--- CanHazFunny/CanHazFunny/IJokeOutput.cs
+++ CanHazFunny/CanHazFunny/IJokeOutput.cs
@@ -8,6 +8,6 @@ namespace CanHazFunny
 {
     public interface IJokeOutput
     {
-        public void Output(string joke); 
+        void Output(string joke); 
     }
 }
--- CanHazFunny/CanHazFunny/IJokeService.cs
+++ CanHazFunny/CanHazFunny/IJokeService.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-[assembly: CLSCompliant(true)]
+
 namespace CanHazFunny
 {
     public interface IJokeService
     {
-        public string GetJoke(); 
+        string GetJoke(); 
     }
 }
