31d8ad1cf82cb7c9d49d17fcea5ddf8ef43ab4d5
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-05 11:45:38 -0800

    Added Tests for the FullName Record

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Tests
+{
+    [TestClass]
+    public class FullNameTests
+    {
+
+        [TestMethod]
+        public void InstantiateFullNameObject_ReturnTrueIfNotNull()
+        {
+            Assert.IsNotNull(new FullName("Billy", "Joe", null));
+
+        }
+
+        [TestMethod]
+        public void InstantiateFullNameObjectWithMiddleName_ReturnTrueIfNotNull()
+        {
+            Assert.IsNotNull(new FullName("Billy", "Joe", "Bob"));
+
+        }
+
+        [TestMethod]
+        public void FullNameComparesByValue_AssertTrueIfObjectsAreEqual()
+        {
+            FullName name1 = new("Johnny", "Smith", "J");
+            FullName name2 = new("Johnny", "Smith", "J");
+            Assert.AreEqual(name1, name2);
+
+        }
+    }
+}
