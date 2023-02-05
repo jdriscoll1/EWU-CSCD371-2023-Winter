7934ad2c79555fb312d2b295d3d5bdcf4547888e
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-05 12:52:28 -0800

    Added First Storage Class Unit Test

--- Logger.Tests/FullNameTests.cs
+++ Logger.Tests/FullNameTests.cs
@@ -56,6 +56,25 @@ namespace Logger.Tests
 
         }
 
+        [TestMethod]
+        public void ValidateRecordStored_ReturnTrueIfObjectsSaved()
+        {
+            // Arrange
+            Storage recordStorage = new();
+            string isbn = "12345";
+            FullName author = new("Poe", "Edgar", "Alan");
+            Book bookRecord = new("The Cat In The Hat", author, isbn);
+            Guid bookID = bookRecord.Id; 
+
+            // Act
+            recordStorage.Add(bookRecord);
+            Book storedBook = recordStorage.Get(bookID) as Book ?? throw new ArgumentNullException();
+
+            // Assert
+            Assert.AreEqual<Book>(storedBook, bookRecord); 
+
+        }
+
 
     }
 }
