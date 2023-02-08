59280f65c0c8ab3371911a04dbb4db17e4effcf3
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-08 11:04:28 -0800

    Project Initialized

---
+++ GenericsHomework.Tests/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericsHomework.Tests
+{
+    [TestClass]
+    public class Class1
+    {
+
+    }
+}
---
+++ GenericsHomework/GenericsHomework.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("GenericsHomework.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GenericsHomework.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("7edc98c7-a693-4df7-9a4d-6fe748bc898f")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
