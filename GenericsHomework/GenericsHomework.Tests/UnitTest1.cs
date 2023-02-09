fef095c922151d3f8ad859dc45e47f5cf0d95379
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-09 09:00:44 -0800

    Project Set up

---
+++ GenericsHomework/GenericsHomework.Tests/UnitTest1.cs
@@ -0,0 +1,56 @@
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace GenericsHomework.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateNode()
+        {
+
+
+        }
+        [TestMethod]
+        public void NodeToStringTest() { 
+        
+        }
+
+        [TestMethod]
+        public void CheckIfNextThrowsExceptionIfNull()
+        {
+
+        }
+
+        [TestMethod]
+        public void InstantiateLinkedList()
+        {
+
+        }
+
+        [TestMethod]
+        public void AppendNodeToLinkedList()
+        {
+
+        }
+
+        [TestMethod]
+        public void RemoveNodeFromLinkedList()
+        {
+
+        }
+
+        [TestMethod]
+        public void ContainsLinkedListTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
