df410f1f8346efad0f29b9720e3a391113c64417
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-16 11:29:08 -0800

    Project Setup

---
+++ Calculate/Calculate.Tests/Program.Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
