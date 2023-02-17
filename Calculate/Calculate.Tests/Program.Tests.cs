ef28b9b4946840fc7fc4d6e8a1a4e7d3c17cbe7a
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-17 11:09:48 -0800

    Added test for Program R/W and Calcualator Class

--- Calculate/Calculate.Tests/Program.Tests.cs
+++ Calculate/Calculate.Tests/Program.Tests.cs
@@ -42,5 +42,30 @@ namespace Calculate.Tests
 
         }
 
+        [TestMethod]
+        public void TestCalculateWithProgram() { 
+            Program program = new();
+            using StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+            string? userInput = "2 + 2";
+
+            // Arrange
+            using StringReader stringReader = new(userInput);
+            Console.SetIn(stringReader);
+            string expected = "The result of the following equation: 2 + 2 is 4";
+
+
+
+            userInput = program.ReadLine();
+             
+            int? result = Calculator.TryCalculate(userInput!);
+                
+             
+            program.WriteLine($"The result of the following equation: {userInput} is {result}");
+            Assert.AreEqual<string>(expected, stringWriter.ToString().Trim()); 
+
+
+        }
+
     }
 }
