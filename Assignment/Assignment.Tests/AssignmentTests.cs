fe3c9f4309c304fa928418dbec24b659c7f6dae1
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-23 20:19:36 -0800

    Created test for reading the file

---
+++ Assignment/Assignment.Tests/AssignmentTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Assignment; 
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class AssignmentTests
+    {
+
+
+
+        [TestMethod]
+        public void TestReadCSV()
+        {
+            SampleData sampleData = new();
+            IEnumerable<string> csvRows = sampleData.CsvRows;
+            IEnumerator<string> csvEnumerator = csvRows.GetEnumerator();
+
+            using StreamReader sr = new("People.csv");
+            string line;
+         
+            while ((line = sr.ReadLine()!) != null && csvEnumerator.MoveNext())
+            {
+                Assert.AreEqual<string>(line, csvEnumerator.Current);
+            
+            }
+        }
+    }
+}
