bb4961056027a923dcfbccfe958782e966b925f5
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-02-26 17:15:36 -0800

    Added Node Tester & Node, Added Test To Validate Enumerator worked and although it implements ICollection ICollection Extends IEnumerable thus it extends IEnumerable. Put in test that uses the LINQ Any Operator to prove this; Began work on test for ChildItems

---
+++ Assignment/Assignment.Tests/NodeTests.cs
@@ -0,0 +1,297 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class NodeTests
+    {
+        private Node<string>? TestNode { get; set; } = null;
+
+        [TestInitialize]
+        public void InstantiateNode()
+        {
+            TestNode = new("myString");
+        }
+        [TestMethod]
+        public void ValidateListWithSizeOneNextEqualsThis()
+        {
+            Assert.IsNotNull(TestNode);
+            Assert.AreEqual<int>(TestNode!.Count, 1);
+            Assert.AreEqual<Node<string>>(TestNode, TestNode.Next);
+
+        }
+        [TestMethod]
+        public void ValidateListWithSizeTwoNextEqualsThis()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+
+            // Act
+            TestNode!.Append("any ol' string");
+
+            // Assert
+            Assert.AreEqual<int>(TestNode.Count, 2);
+            Assert.AreEqual<Node<string>>(TestNode, TestNode.Next.Next);
+
+        }
+        [TestMethod]
+        public void AppendNodeTest()
+        {
+            Assert.IsNotNull(TestNode);
+            TestNode!.Append("Test Node");
+            Assert.IsTrue(TestNode.Exists("Test Node"));
+        }
+        [TestMethod]
+        public void ValidateToArray()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+            TestNode!.Append("1");
+            TestNode.Append("2");
+            TestNode.Append("3");
+            TestNode.Append("4");
+            TestNode.Append("5");
+            TestNode.Append("6");
+            TestNode.Append("7");
+            // Collection assert does not have a generic override
+            CollectionAssert.AreEqual(new string[] { "myString", "7", "6", "5", "4", "3", "2", "1" }, TestNode.ToArray());
+        }
+        [TestMethod]
+        public void ClearNodes()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+            TestNode!.Append("1");
+            TestNode.Append("2");
+            TestNode.Append("3");
+            TestNode.Append("4");
+            TestNode.Append("5");
+            TestNode.Append("6");
+            TestNode.Append("7");
+            CollectionAssert.AreEqual(new string[] { "myString", "7", "6", "5", "4", "3", "2", "1" }, TestNode.ToArray());
+            // Act
+            TestNode.Clear();
+            // Assert
+            CollectionAssert.AreEqual(new string[] { "myString" }, TestNode.ToArray());
+        }
+        [TestMethod]
+        public void AssertContainsReturnsFalse()
+        {
+            Assert.IsNotNull(TestNode);
+            Assert.IsFalse(TestNode!.Contains("1"));
+            Assert.IsFalse(TestNode!.Exists("1"));
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AssertDuplicatesCannotBeAddedNode()
+        {
+            Assert.IsNotNull(TestNode);
+            TestNode!.Append("1");
+            TestNode.Append("1");
+        }
+
+        [TestMethod]
+        public void ContainsLinkedListTestHead()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+            Assert.IsTrue(TestNode!.Exists("myString"));
+        }
+        [TestMethod]
+        public void ExistsNodeTestBody()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+
+            TestNode!.Append("1");
+            TestNode.Append("2");
+            TestNode.Append("3");
+            Assert.IsTrue(TestNode.Exists("2"));
+
+        }
+
+        [TestMethod]
+        public void ContainsNodeTestBody()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+
+            TestNode!.Add("1");
+            TestNode.Add("2");
+            TestNode.Add("3");
+            Assert.IsTrue(TestNode.Contains("2"));
+        }
+
+        [TestMethod]
+        public void ValidateNodeToString()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+            string expected = "myString";
+            // Assert
+            Assert.AreEqual<string>(expected, TestNode!.ToString());
+        }
+        [TestMethod]
+        public void ValidateCountReturnsNumberOfItems()
+        {
+            Assert.IsNotNull(TestNode);
+            Assert.AreEqual<int>(TestNode!.Count(), 1);
+            TestNode!.Append("1");
+            Assert.AreEqual<int>(TestNode.Count(), 2);
+            TestNode.Append("2");
+            Assert.AreEqual<int>(TestNode.Count(), 3);
+            TestNode.Append("3");
+            Assert.AreEqual<int>(TestNode.Count(), 4);
+            TestNode.Append("4");
+            Assert.AreEqual<int>(TestNode.Count(), 5);
+            TestNode.Clear();
+            Assert.AreEqual<int>(TestNode.Count(), 1);
+        }
+        [TestMethod]
+        public void ConvertLinkedListToArray_StartAt0()
+        {
+            Assert.IsNotNull(TestNode);
+            TestNode!.Add("1");
+            TestNode.Add("2");
+            string[] arr = { "3", "4", "5" };
+            TestNode.CopyTo(arr, 0);
+            CollectionAssert.AreEqual(arr, TestNode.ToArray());
+        }
+        [TestMethod]
+        public void ConvertLinkedListToArray_StartAt1()
+        {
+            Assert.IsNotNull(TestNode);
+            TestNode!.Add("1");
+            TestNode.Add("2");
+            string[] arr = { "3", "4", "5" };
+            TestNode.CopyTo(arr, 1);
+            CollectionAssert.AreEqual(new string[] { "3", "myString", "2" }, arr);
+        }
+        [TestMethod]
+        public void ConvertLinkedListToArray_StartAt2()
+        {
+            Assert.IsNotNull(TestNode);
+            TestNode!.Add("1");
+            TestNode.Add("2");
+            string[] arr = { "3", "4", "5" };
+            TestNode.CopyTo(arr, 2);
+            Assert.AreEqual<string>("3 4 myString", string.Join(" ", arr));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ValidateCopyToThrowNullException()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+            string[] nullArray = null!;
+            // Act
+            TestNode!.CopyTo(nullArray, 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ValidateCopyToThrowOutofRangeException()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+            string[] nullArray = { "1", "2" };
+            // Act
+            TestNode!.CopyTo(nullArray, 3);
+        }
+        [TestMethod]
+        public void TestEnumerator_ReturnTrueIfOutputIntoString()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+            // Act
+            TestNode!.Append("0");
+            TestNode.Append("1");
+            TestNode.Append("2");
+            TestNode.Append("3");
+
+            string actual = "";
+            // Assert
+            foreach (string value in TestNode)
+            {
+                actual += value;
+            }
+            string expected = "myString3210";
+            Assert.AreEqual<string>(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestRemoveFromLinkedList()
+        {
+            // Arrange
+            Assert.IsNotNull(TestNode);
+            TestNode!.Append("1");
+            TestNode.Append("2");
+            TestNode.Append("3");
+            CollectionAssert.AreEqual(new string[] { "myString", "3", "2", "1" }, TestNode.ToArray());
+
+            // Act
+            Assert.IsTrue(TestNode.Remove("2"));
+
+            // Assert
+            CollectionAssert.AreEqual(new string[] { "myString", "3", "1" }, TestNode.ToArray());
+
+            // Act
+            Assert.IsTrue(TestNode.Remove("3"));
+
+            // Assert
+            CollectionAssert.AreEqual(new string[] { "myString", "1" }, TestNode.ToArray());
+
+            Assert.IsFalse(TestNode.Remove("myString"));
+
+            Assert.IsFalse(TestNode.Remove("2"));
+        }
+
+        [TestMethod]
+        public void Test_NodeImplementsIEnumerator()
+        {
+            TestNode!.Append("1");
+            TestNode!.Append("2");
+            Assert.AreEqual<int>(3, TestNode.Count);
+            Assert.IsNotNull(TestNode);
+            TestNode?.Any(element => element is not null);
+
+        }
+
+        [TestMethod]
+        public void Test_ChildItems_MaxIsLessThanTotal()
+        {
+            
+            // Arrange
+            TestNode!.Append("1");
+            TestNode!.Append("2");
+            TestNode!.Append("3");
+            TestNode!.Append("4");
+            TestNode!.Append("5");
+
+            // Act
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void Test_ChildItems_MaxIsGreaterThanTotal()
+        {
+            // Arrange
+            TestNode!.Append("1");
+            TestNode!.Append("2");
+
+            // Act
+
+            // Assert
+
+
+        }
+    }
+}
---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    // ICollection extends IEnumerable, so therefore this implements IEnumerable
+    public class Node<TNodeType> : ICollection<TNodeType>
+    {
+        private Node<TNodeType>? _Next { get; set; }
+
+        private TNodeType? _Value;
+
+        public TNodeType? Value
+        {
+            get
+            {
+                return _Value;
+            }
+            private set
+            {
+                _Value = value;
+            }
+        }
+        public Node<TNodeType> Next
+        {
+            get
+            {
+                return _Next!;
+            }
+            private set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                _Next = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                int count = 0;
+                foreach (TNodeType element in this)
+                {
+                    count++;
+                }
+                return count;
+            }
+        }
+        bool ICollection<TNodeType>.IsReadOnly { get; } = false;
+        public Node(TNodeType value)
+        {
+            Value = value;
+            Next = this;
+        }
+
+        private Node(TNodeType value, Node<TNodeType> next)
+        {
+            Value = value;
+            Next = next;
+        }
+
+        public void Append(TNodeType data)
+        {
+            if (Exists(data))
+            {
+                throw new ArgumentException($"{data} is already added in list");
+            }
+            Node<TNodeType> newNode = new(data, Next);
+            Console.WriteLine(newNode.Value);
+            Next = newNode;
+        }
+
+        public bool Exists(TNodeType data)
+        {
+            Node<TNodeType> head = this;
+            Node<TNodeType> curr = head;
+            do
+            {
+                if (EqualityComparer<TNodeType>.Default.Equals(data, curr.Value))
+                {
+                    return true;
+                }
+                curr = curr.Next;
+
+            } while (curr != head);
+            return false;
+        }
+
+        public TNodeType[] ToArray()
+        {
+            TNodeType[] array = new TNodeType[Count];
+            int i = 0;
+            foreach (TNodeType element in this)
+            {
+                array[i] = element;
+                i++;
+            }
+            return array;
+        }
+
+        public override string ToString()
+        {
+            return Value?.ToString() ?? "null";
+        }
+
+        public void Clear()
+        {
+            // It is sufficient to set the current node's next to itself, this will allow the garbage collector to clean the whole linked list. 
+            // We don't need to go through each individual and close the loop on it, setting it to itself
+            // This is becaues the garbage collector will recognize that the first
+            // Item in the list has no reference pointing to it, and will clean it, then look at the next one. This will occur recursively 
+            // We tested to see if the Garbage Collector arrives sooner if each reference points to itself; however, the destructor
+            // showed no signs of enhancement
+            // We don't need the removed items to circle back on themselves because if they have a reference to themselves they may not be collected
+            // Thus they cannot be in a a circle, because if there is a circular list of items, you will have to worry about garbage collection at least until the program closes
+
+            Next = this;
+        }
+
+        public void Add(TNodeType item)
+        {
+            Append(item);
+        }
+        bool ICollection<TNodeType>.Contains(TNodeType item)
+        {
+            return Exists(item);
+        }
+
+        public void CopyTo(TNodeType[] array, int arrayIndex)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            if (arrayIndex > array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            }
+            Node<TNodeType> head = this;
+            Node<TNodeType> curr = this;
+
+            int index = arrayIndex;
+            do
+            {
+                if (array.Length > index)
+                {
+                    array[index] = curr.Value!;
+                }
+                else
+                {
+                    array.Append(curr.Value);
+                }
+                curr = curr.Next;
+                index++;
+            } while (curr != head);
+        }
+
+        public bool Remove(TNodeType item)
+        {
+
+            Node<TNodeType> curr = this;
+            Node<TNodeType> prev = this;
+            Node<TNodeType> head = this;
+
+            do
+            {
+                if (EqualityComparer<TNodeType>.Default.Equals(item, curr.Value))
+                {
+                    // Cannot remove the head object 
+                    if (curr == head)
+                    {
+                        return false;
+                    }
+
+                    // Without a reference to curr, the garbage collector will remove it 
+                    prev.Next = curr.Next;
+                    return true;
+                }
+                prev = curr;
+                curr = curr.Next;
+
+            } while (curr != head);
+            return false;
+        }
+
+        IEnumerator<TNodeType> IEnumerable<TNodeType>.GetEnumerator()
+        {
+            Node<TNodeType> curr = this;
+            do
+            {
+                yield return curr.Value!;
+                curr = curr.Next;
+            } while (curr != this);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            Node<TNodeType> curr = this;
+            do
+            {
+                yield return curr.Value!;
+                curr = curr.Next;
+            } while (curr != this);
+
+        }
+    }
+}
